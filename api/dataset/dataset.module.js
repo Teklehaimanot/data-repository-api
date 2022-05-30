c942641dcf41dbc1f03199a0c4b125da03f87ca2
Teklehaimanot Belete <enbish.blt@gmail.com>
2022-05-30 20:18:05 +0300

    dataset schema is created

---
+++ api/dataset/dataset.module.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const datasetSchema = {
+    dataset_name: {
+        type: String,
+        required: true
+    },
+    data_type: {
+        type: String,
+        required: true
+    },
+    sample_size: {
+        type: Number,
+        required: true
+    },
+    area_coverage: {
+        type: String,
+        required: true
+    },
+    sex_coverage: {
+        type: String,
+        required: true
+    },
+    data_collection_start_date: {
+        type: Date,
+        required: true
+    },
+    data_collection_end_date: {
+        type: Date,
+        required: true
+    },
+    study_type: {
+        type: String,
+        required: true
+    }
+}
+
+module.exports = mongoose.model('dataset', datasetSchema)
